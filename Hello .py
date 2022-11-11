6f0dcee80a71331f2582c01568ef7cfd7077ba7c
Smitesh964 <117960251+Smitesh964@users.noreply.github.com>
2022-11-11 14:47:54 +0530

    Update Hello .py

--- Hello .py
+++ Hello .py
@@ -1,1 +1,7 @@
-print("Hello world")
+print("Hello World")
+("Hello world")
+#this program adds two numbers 
+num1=1.2
+num2=3.5
+sum=num1+num2
+print('the sum of{0} and {1} is {2}'.format,num1,num2,sum))
